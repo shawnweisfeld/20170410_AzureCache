14e41c79047ebcea67d52aa319c8491ed3c861c5
Shawn Weisfeld <shawn@shawnweisfeld.com>
2017-04-09 22:24:00 -0500

    added pptx

--- CacheDemo/CacheDemo/App_Start/BundleConfig.cs
+++ CacheDemo/CacheDemo/App_Start/BundleConfig.cs
@@ -8,22 +8,27 @@ namespace CacheDemo
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.UseCdn = true;
+            var version = System.Reflection.Assembly.GetAssembly(typeof(Controllers.HomeController))
+              .GetName().Version.ToString();
+            var cdnUrl = "//sweisfelcachecdn.azureedge.net/{0}?" + version;
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", string.Format(cdnUrl, "bundles/jquery")).Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval", string.Format(cdnUrl, "bundles/jqueryval")).Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/bundles/modernizr", string.Format(cdnUrl, "bundles/modernizr")).Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", string.Format(cdnUrl, "bundles/bootstrap")).Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css", string.Format(cdnUrl, "Content/css")).Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
